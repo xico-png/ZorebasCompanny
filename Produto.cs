4f9e4cdb7c599a4a34b44a05e94bbfa3fd634677
xico-png <luizfranxico1010@gmail.com>
2023-08-24 09:26:36 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZorebasCompanny
+{
+    class Produto
+    {
+        private string nome;
+        private double preco;
+        private int id;
+        private int quantidadedeprodutos; 
+
+
+
+        public Produto(string nome, float preco, int id, int quantidadedeprodutos)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.id = id;
+            this.quantidadedeprodutos = quantidadedeprodutos;
+        }
+        public void SetPreco(double preco)
+        {
+            this.preco = preco;
+        }
+        public double GetPreco()
+        {
+            return preco;
+        }
+        public void SetNome(string nomedoproduto)
+        {
+           this.nome = nomedoproduto;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public void SetId(int iddoproduto)
+        {
+            this.id = iddoproduto;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetQuantidade(int quantia)
+        {
+            this.quantidadedeprodutos = quantia;
+        }
+        public int GetQuantia()
+        {
+            return quantidadedeprodutos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+ namespace ZorebasCompanny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ZorebasCompanny");
+            Console.WriteLine("Você tem um novo produto");
+            List<Produto> listdeprodutos = new();
+            Produto resistores = new("Resistores", 0.2f, 10, 10);
+            listdeprodutos.Add(resistores);
+            
+
+
+        }
+    }
+}
